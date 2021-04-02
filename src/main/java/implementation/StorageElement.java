4743cb2e6946380c1d14c453df925283fe8b4a3e
Tõnis Prants <tonis.prants7@gmail.com>
2021-04-02 14:18:32 +0300

    Added minor tests for storageElement and cleanup function for it

--- src/main/java/implementation/StorageElement.java
+++ src/main/java/implementation/StorageElement.java
@@ -1,9 +1,14 @@
 package implementation;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.List;
 
 public class StorageElement<Key, Value> {
     private Hashtable<Key, ValueAndExpirationPair<Value>> storageLocation;
+    private LocalTime lastCleanUpTime;
 
     public StorageElement() {
         this.storageLocation = new Hashtable<>();
@@ -15,5 +20,46 @@ public class StorageElement<Key, Value> {
 
     public ValueAndExpirationPair<Value> get(Key keyElement) {
         return this.storageLocation.get(keyElement);
+    }
+
+    public void performCleanUp(LocalTime currentTime) {
+        List<Key> expiredKeys = this.getListOfKeysWithExpiredValues(currentTime);
+        this.performRemovalOfExpiredValues(expiredKeys, currentTime);
+        this.lastCleanUpTime = currentTime;
+    }
+
+    private List<Key> getListOfKeysWithExpiredValues(LocalTime currentTime) {
+        List<Key> expiredKeys = new ArrayList<>();
+        Enumeration<Key> keys = this.storageLocation.keys();
+        while(keys.hasMoreElements()) {
+            Key nextKey = keys.nextElement();
+            ValueAndExpirationPair<Value> nextValue = storageLocation.get(nextKey);
+            if(nextValue.getExpirationMoment().isBefore(currentTime)) {
+                expiredKeys.add(nextKey);
+            }
+        }
+        return expiredKeys;
+    }
+
+    private void performRemovalOfExpiredValues(List<Key> expiredKeys, LocalTime currentTime) {
+        for(Key oneKey : expiredKeys) {
+            performRemovalOfOneExpiredKey(oneKey, currentTime);
+        }
+    }
+
+    private boolean performRemovalOfOneExpiredKey(Key oneKey, LocalTime currentTime) {
+        if(this.storageLocation.get(oneKey).getExpirationMoment().isBefore(currentTime)) {
+            this.storageLocation.remove(oneKey);
+            return true;
+        }
+        return false;
+    }
+
+    public int getStorageSize() {
+        return this.storageLocation.size();
+    }
+
+    public LocalTime getLastCleanUpTime() {
+        return this.lastCleanUpTime;
     }
 }
---
+++ src/test/java/extraTests/StorageElementTest.java
@@ -0,0 +1,77 @@
+package extraTests;
+
+import implementation.StorageElement;
+import implementation.ValueAndExpirationPair;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class StorageElementTest {
+
+    public StorageElement<String, String> getTestableStorageElement() {
+        return new StorageElement<String, String>();
+    }
+
+    public ValueAndExpirationPair<String> getTestTableValueAndExpirationPair(
+            String valueString,
+            LocalTime expirationTime) {
+        return new ValueAndExpirationPair<>(valueString, expirationTime);
+    }
+
+    @Test
+    public void putAndGetWorkForStorageElement() {
+        StorageElement<String, String> testableElement = this.getTestableStorageElement();
+        ValueAndExpirationPair<String> testValueAndExpirationPair =
+                this.getTestTableValueAndExpirationPair("value", LocalTime.now());
+        testableElement.put("key", testValueAndExpirationPair);
+        assertEquals(testValueAndExpirationPair, testableElement.get("key"));
+    }
+
+    @Test
+    public void canGetStorageElementSize() {
+        StorageElement<String, String>  testableElement = this.getTestableStorageElement();
+        assertEquals(0, testableElement.getStorageSize());
+        testableElement.put("key",
+                this.getTestTableValueAndExpirationPair("value", LocalTime.now()));
+        testableElement.put("key2",
+                this.getTestTableValueAndExpirationPair("value2", LocalTime.now()));
+        assertEquals(2, testableElement.getStorageSize());
+    }
+
+    @Test
+    public void canPerformStorageCleanupThatRemovesExpiredValues() {
+        StorageElement<String, String>  testableElement = this.getTestableStorageElement();
+        LocalTime checkTime = LocalTime.now();
+        LocalTime expiredTime = checkTime.minus(10, ChronoUnit.SECONDS);
+        LocalTime notExpiredTime = checkTime.plus(10, ChronoUnit.SECONDS);
+
+        testableElement.put("expiredKey",
+                this.getTestTableValueAndExpirationPair("value", expiredTime));
+        testableElement.put("freshKey",
+                this.getTestTableValueAndExpirationPair("value", notExpiredTime));
+        assertEquals(2, testableElement.getStorageSize());
+        testableElement.performCleanUp(checkTime);
+        assertEquals(1, testableElement.getStorageSize());
+    }
+
+    @Test
+    public void willSaveAndChangeLastCleanUpTime() {
+        StorageElement<String, String>  testableElement = this.getTestableStorageElement();
+
+        assertNull(testableElement.getLastCleanUpTime());
+
+        LocalTime checkTime1 = LocalTime.now();
+        testableElement.performCleanUp(checkTime1);
+        assertEquals(checkTime1, testableElement.getLastCleanUpTime());
+
+        LocalTime checkTime2 = checkTime1.plus(10, ChronoUnit.SECONDS);
+        testableElement.performCleanUp(checkTime2);
+        assertEquals(checkTime2, testableElement.getLastCleanUpTime());
+
+
+    }
+}
